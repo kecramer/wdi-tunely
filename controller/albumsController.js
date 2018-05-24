2749a4c93627542ab3ed40c34dcb47c7bd3091bf
kecramer <kcramer@gmail.com>
2018-05-24 11:13:18 -0700

    actually commit controller stubs

---
+++ controller/albumsController.js
@@ -0,0 +1,27 @@
+function index(req, res) {
+
+};
+
+function create(req, res) {
+
+};
+
+function show(req, res) {
+
+};
+
+function destroy(req, res) {
+
+};
+
+function update(req, res) {
+
+};
+
+module.exports = {
+   index,
+   create,
+   show,
+   destroy,
+   update,
+}
