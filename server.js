195f2a34ad6e8a49365204b56944cbbf8abc6276
kecramer <kcramer@gmail.com>
2018-05-24 10:48:26 -0700

    Fix whitespace

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express'),
 
 var app = express();
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
 	res.sendFile('views/index.html' , {root : __dirname});
