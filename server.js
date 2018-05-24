185289da75fd079b3494c83b6f91d2db164eb9bf
kecramer <kcramer@gmail.com>
2018-05-24 10:47:02 -0700

    Modify project to server from node instead of static html

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express'),
+    bodyParser = require('body-parser');
+
+var app = express();
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+	res.sendFile('views/index.html' , {root : __dirname});
+});
+
+app.listen(process.env.PORT || 3000, () => {
+	console.log('Book app listening at http://localhost:3000/');
+});
